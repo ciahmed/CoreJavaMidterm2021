a6afac5df415e36261844f1b4fac84a3ee8d07f9
“ahmed” <chowad@gmail.com>
2021-10-22 07:39:30 -0700

    Anagram/DuplicateWord/Palindrome/Permutation/UnitTestingStringProblem Done

--- src/problems/UnitTestingStringProblem.java
+++ src/problems/UnitTestingStringProblem.java
@@ -2,20 +2,38 @@ package problems;
 
 import org.testng.Assert;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class UnitTestingStringProblem {
     public static void main(String[] args) {
+
         //Apply Unit Test into all the methods in this package.
 
         //Anagram
 
+        try {
+            Assert.assertEquals(Anagram.isAnagram("rat", "tar"), true);
+            System.out.println("Anagram Unit Test Passed.");
+        } catch (AssertionError as) {
+            System.out.println("Anagram Unit Test Failed!!!!");
+        }
+
 
         //Duplicate word
 
+        try {
+            Set<String> set = new HashSet<>();
+            set.add("Java");
+            set.add("is");
+            String test = "Java is a programming Language. Java is also an Island of Indonesia";
+            Assert.assertEquals(DuplicateWord.duplicateWords(test), set);
+            System.out.println("Duplicate Word Unit Test Passed.");
+        } catch (AssertionError as) {
+            System.out.println("Duplicate Word Unit Test Failed!!!.");
+        }
 
-        //Palindrome
 
-
-        //Permutation
 
     }
 }
