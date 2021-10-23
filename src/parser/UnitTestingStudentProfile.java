8327dc09ef5378f147bdeac884bb7dd584e64d83
“ahmed” <chowad@gmail.com>
2021-10-22 20:19:53 -0700

    Done

--- src/parser/UnitTestingStudentProfile.java
+++ src/parser/UnitTestingStudentProfile.java
@@ -1,11 +1,59 @@
 package parser;
 
+import org.testng.Assert;
+
 public class UnitTestingStudentProfile {
     public static void main(String[] args) {
         //Apply Unit Test into all the methods in this package.
 
-        //implementation here...
+        Student student = new Student("Chowdhury", "Ahmed", "A", "8505CA");
+
+        try {
+            Assert.assertEquals(student.getFirstName(), "Chowdhury");
+            System.out.println("Test Passed getFirstName");
+        } catch (AssertionError as) {
+            System.out.println("Test Failed getFirstName");
+            System.out.println(as.getMessage());
+            System.out.println(as.getStackTrace());
+        }
+
+        try {
+            Assert.assertEquals(student.getLastName(), "Ahmed");
+            System.out.println("Test passed lastName");
+        } catch (AssertionError as) {
+            System.out.println("Test Failed lastName");
+            System.out.println(as.getMessage());
+            System.out.println(as.getStackTrace());
+        }
+
+        try {
+            Assert.assertEquals(student.getId(), "8505CA");
+            System.out.println("Test passed getId");
+        } catch (AssertionError as) {
+            System.out.println("Test Failed getId");
+            System.out.println(as.getMessage());
+            System.out.println(as.getStackTrace());
+        }
+
+        try {
+            Assert.assertEquals(student.getScore(), "A");
+            System.out.println("Test passed getScore");
+        } catch (AssertionError as) {
+            System.out.println("Test Failed getScore");
+            System.out.println(as.getMessage());
+            System.out.println(as.getStackTrace());
+        }
+
+        try {
+            Assert.assertEquals(student.toString(), "Student(id= 8505CA)Chowdhury Ahmed  Grade = A");
+            System.out.println("Test passed toString");
+        } catch (AssertionError as) {
+            System.out.println("Test Failed toString");
+            System.out.println(as.getMessage());
+            System.out.println(as.getStackTrace());
 
 
+        }
     }
+
 }
